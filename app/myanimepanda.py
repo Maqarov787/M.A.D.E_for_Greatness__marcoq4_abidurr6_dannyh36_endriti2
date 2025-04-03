428951866ee918a10122d1214cbf680fb5c6920e
Marco Quintero <mquintero50@stuy.edu>
2025-04-03 00:16:32 -0400

    started working on mutator methods for data cleaning. Only method that is a work in progress if filter_chars()

--- app/myanimepanda.py
+++ app/myanimepanda.py
@@ -1,13 +1,42 @@
 import pandas as pd
 anime = pd.read_csv("static/anime.csv")
 
-def anime_info():
-    anime.info(verbose = False)
+#Mutator methods
+def keep_500():
+    dropped_anime = anime[anime["popularity"] > 500].index
+    anime.drop(dropped_anime, inplace=True)
 
-def keep_1000():
-    for x in anime.index:
-        if anime.loc[x, "popularity"] > 1000:
-            anime.drop(anime.loc[anime["popularity"]==anime.loc[x, "popularity"]].index, inplace=True)
+def remove_nsfw():
+    nsfw_anime = anime[anime["nsfw"] != "white"].index
+    anime.drop(nsfw_anime, inplace=True)
 
-keep_1000()
-anime_info()
+'''def filter_chars():
+#Will filter out special characters in any of the data.
+    invalid_chars = anime[anime["title"].str.match("^[a-zA-Z0-9 ]")].index
+    anime.drop(invalid_chars, inplace=True)'''
+
+def drop_columns():
+    anime.drop(["id", "created_at", "updated_at", "alternative_titles_en", "alternative_titles_ja", "alternative_titles_synonyms"], axis=1, inplace=True)
+
+def remove_zero_mean():
+    zero_values = anime[anime["mean"] == 0].index
+    anime.drop(zero_values, inplace=True)
+
+def remove_zero_popularity():
+    zero_values = anime[anime["popularity"] == 0].index
+    anime.drop(zero_values, inplace=True)
+
+def sort():
+    anime.sort_values(by=["popularity"], inplace=True)
+
+#Accessor Methods
+remove_zero_mean()
+remove_zero_popularity()
+drop_columns()
+remove_nsfw()
+#filter_chars()
+keep_500()
+sort()
+print(len(anime))
+#print(anime.to_string())
+print(anime.iloc[0:50, 8:12])
