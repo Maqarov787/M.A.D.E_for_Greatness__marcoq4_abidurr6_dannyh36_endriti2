18d492f6f2de9ba7c4c1231799c4e2f23a6c0ec7
Danny Huang <dhuang50@stuy.edu>
2025-04-09 11:43:32 -0400

    Finished routing for loginand signup, fixed db methods

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,17 @@ Target Ship Date: 2025-04-21
 '''
 
 
+import os
 from flask import Flask
 from flask import render_template  
 from flask import request           
 from flask import session
+from flask import redirect
+import myanimedb as db
 
 app = Flask(__name__)
+secret = os.urandom(32)
+app.secret_key = secret
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -27,21 +32,50 @@ def filter():
 def graph():
     return render_template("graph.html")     
 
-@app.route("/profile", methods=['GET', 'POST'])
-def profile():
+@app.route("/profile/<username>", methods=['GET', 'POST'])
+def profile(username):
     return render_template("profile.html")
     
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
+    if  'username' in session.keys() and session['username'] is not None:
+        return redirect('/')
+    elif request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('pw')
+        if db.getUserName(username) is None:
+           return render_template("signin.html", message="This user does not exist")
+        if db.getPassword(username) != password:
+           return render_template("signin.html", message="Incorrect Password")
+        session['username'] = username
+        session['password'] = password
+        return redirect('/')
     return render_template("signin.html")
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if  'username' in session.keys() and session['username'] is not None:
+        return redirect('/')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['pw']
+        if db.getUserName(username) is None:
+            db.addUser(username, password)
+            return redirect('/signin')
+        else:
+            return render_template('signup.html', message="Username already exists")
     return render_template("signup.html")
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect('/')
 
 @app.route("/taste", methods=['GET', 'POST'])
 def taste():
     return render_template("taste.html")
+
 
 if __name__ == "__main__": 
     app.debug = True      
--- app/myanimedb.py
+++ app/myanimedb.py
@@ -22,7 +22,7 @@ def addUser(username, password):
     c = db.cursor()
     c.execute("SELECT userID FROM userData;")
     num = c.fetchall()
-    c.execute("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?);", (len(num), username, password))
+    c.execute("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", (len(num), username, password))
 
     db.commit()
     db.close()
@@ -41,7 +41,7 @@ def addGraph(userID, file_name):
             c.execute(f"ALTER TABLE userData ADD graph{last} INTEGER")
             last = last + 1
 
-        c.execute(f"SELECT graph{i} FROM userData WHERE userID = {userID}")
+        c.execute(f"SELECT graph{i} FROM userData WHERE userID = '{userID}'")
         graph = c.fetchone()
         print("Current graph: " + str(graph))
         if list(graph)[0] is None:
@@ -70,28 +70,31 @@ def allUserData():
     print("users: ")
     print(prin)
 
-def getUserName(userID):
+def getUserName(username):
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
-    res = c.execute(f"SELECT username FROM userData where userID = {userID}")
+    res = c.execute(f"SELECT username FROM userData where username = '{username}'")
+    try:
+        fin = res.fetchone()[0]
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        fin = None
+    db.commit()
+    db.close()
+    return fin
+def getPassword(username):
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)
+    c = db.cursor()
+    res = c.execute(f"SELECT password FROM userData where username = '{username}'")
     fin = list(res.fetchone())[0]
 
     db.commit()
     db.close()
     return fin
-def getPassword(userID):
+def getGraph(username, colNum):
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
-    res = c.execute(f"SELECT password FROM userData where userID = {userID}")
-    fin = list(res.fetchone())[0]
-
-    db.commit()
-    db.close()
-    return fin
-def getGraph(userID, colNum):
-    db = sqlite3.connect(DB_FILE, check_same_thread=False)
-    c = db.cursor()
-    res = c.execute(f"SELECT graph{colNum} FROM userData where userID = {userID}")
+    res = c.execute(f"SELECT graph{colNum} FROM userData where username = '{username}'")
     fin = list(res.fetchone())[0]
 
     db.commit()
@@ -99,14 +102,15 @@ def getGraph(userID, colNum):
     return fin
 
 #Testing Area
-createTable()
-addUser("maq", "787")
-addUser("sunjinwoo", "arise")
-#addGraphColumn()
-#addGraphColumn()
-addGraph(0, "test.jpg")
-addGraph(0, "test2.jpg")
-addGraph(1, "pie.jpg")
-print("Should be: maq. Result: " + str(getUserName(0)))
-print("Should be: arise. Result: " + str(getPassword(1)))
-allUserData()
+# createTable()
+# addUser("maq", "787")
+# addUser("sunjinwoo", "arise")
+# #addGraphColumn()
+# #addGraphColumn()
+# addGraph(0, "test.jpg")
+# addGraph(0, "test2.jpg")
+# addGraph(1, "pie.jpg")
+# print("Should be: maq. Result: " + str(getUserName(0)))
+# print("Should be: arise. Result: " + str(getPassword(1)))
+# allUserData()
+getUserName("maq")
