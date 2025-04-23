2f6b55020e2a62679ed984b358e4ca1258334f57
EndritIdrizi <eidrizi50@stuy.edu>
2025-04-22 20:23:43 -0400

    working on graph

--- app/__init__.py
+++ app/__init__.py
@@ -57,7 +57,7 @@ def graph():
     if request.method =='POST':
         graph = request.form.get('typeOfGraph')
         category = request.form.get('category')
-        values = request.form.getlist('values')
+        values = request.form.get('values')
         print(graph)
         print(category)
         print(values)
@@ -65,14 +65,14 @@ def graph():
             if (graph == 'line'):
                 return render_template("graph.html", username = session['username'], loggedIn="true", __type__ = graph) #__label_array__ will be the names of the anime in each category, __data_array will be their ratings
             if (graph == 'bar'):
-                return render_template("graph.html", username = session['username'], loggedIn="true", __type__ = graph, __label_array__ = category) #__data_array__ will be the amount of animes in each category
+                return render_template("graph.html", username = session['username'], loggedIn="true", __type__ = graph, __label_array__ = category, __label__ = 'yohoo', __data_array__ = []) #__data_array__ will be the amount of animes in each category
             if (graph == 'pie'):
                 return render_template("graph.html", username = session['username'], loggedIn="true", __type__ = graph, __label_array__ = category) #
         else:
             if (graph == 'line'):
                 return render_template("graph.html", loggedIn="false", __type__ = graph)
             if (graph =='bar'):
-                return render_template("graph.html", loggedIn="false", __type__ = graph, __label_array__ = category)
+                return render_template("graph.html", loggedIn="false", __type__ = graph, __label_array__ = category, __label__ = 'yohoo', __data_array__ = [])
             if (graph == 'pie'):
                 return render_template("graph.html", loggedIn="false", __type__ = graph, __label_array__ = category)
     
